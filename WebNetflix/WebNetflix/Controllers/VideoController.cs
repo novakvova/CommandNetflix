80f946b59c87da68f5802b884ad31445605597f7
Art <Art@TOASTER>
2025-08-13 17:58:23 +0300

    video test

---
+++ WebNetflix/WebNetflix/Controllers/VideoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNetflix.Controllers
+{
+    public class VideoController : Controller
+    {
+        [HttpGet("video")]
+        public IActionResult GetVideo()
+        {
+            var videoPath = Path.Combine("Videos", "sample.mp4");
+            Console.WriteLine(videoPath);
+            var stream = new FileStream(videoPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return File(stream, "video/mp4", enableRangeProcessing: true);
+        }
+    }
+}
