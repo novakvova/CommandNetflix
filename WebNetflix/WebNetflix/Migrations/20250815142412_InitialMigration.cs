05733d74fe8ca639422ee68308fe41569c245d7d
cirillad <152891476+cirillad@users.noreply.github.com>
2025-08-15 17:26:51 +0300

    DataBase

---
+++ WebNetflix/WebNetflix/Migrations/20250815142412_InitialMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebNetflix.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "neondb_owner");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "neondb_owner",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "neondb_owner");
+        }
+    }
+}
---
+++ WebNetflix/WebNetflix/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebNetflix.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
