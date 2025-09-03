30d88748d32b3918e49f57872fdc16ba7d6e6f3c
cirillad2 <burba.2016london@gmail.com>
2025-09-03 17:15:31 +0300

    Genres

---
+++ WebNetflix/WebNetflix/Controllers/GenresController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebNetflix.Data;
+using WebNetflix.Models.Dto;
+
+namespace WebNetflix.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenresController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public GenresController(AppDbContext db) => _db = db;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GenreDto>>> GetGenres()
+        {
+            var genres = await _db.Genres
+                .AsNoTracking()
+                .OrderBy(g => g.Name)
+                .Select(g => new GenreDto { Id = g.Id, Name = g.Name })
+                .ToListAsync();
+
+            return Ok(genres);
+        }
+    }
+}
